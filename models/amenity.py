af9b807647f52216792d80085862ecad70bf1514
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-18 04:55:29 +0200

    2024-04-18 04:55:29 AM

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """this module for class amenity"""
 from models.base_model import BaseModel, Base
-from os import getenv
+import os
 from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
     """class : Amenity to store more data about amenities"""
-    if getenv('HBNB_TYPE_STORAGE') == "db":
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
     else:
