e7958fdc8f3519520c6d764fa914d01bd8b4712e
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-19 14:47:57 +0200

    2024-04-19 02:47:57 PM

--- models/base_model.py
+++ models/base_model.py
@@ -38,7 +38,7 @@ class BaseModel:
 
     def __str__(self):
         """ str reprecentation """
-        return f"[{type(self).__name__}] ({self.id}) {self.to_dict()}"
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """ to save obj in json file """
