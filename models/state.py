54054f3f355e1b8d93a6b8ffd306cef6f14618d0
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-19 15:21:17 +0200

    2024-04-19 03:21:17 PM

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 """this module for class state"""
 from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import String, Column
 import models
 from models.city import City
@@ -14,7 +14,10 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
+        cities = relationship("City",
+                               backref=backref("state", cascade="all"),
+                               cascade="all, delete-orphan",
+                               single_parent=True)
     else:
         name = ""
 
