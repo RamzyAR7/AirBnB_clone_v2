e9623eaed1c725adc6a51ad858d18199fa7447ed
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-19 10:48:02 +0200

    2024-04-19 10:48:02 AM

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ for reload data every process"""
 import os
-storageType = "db"
 
-if os.getenv('HBNB_TYPE_STORAGE') == storageType:
+if os.getenv('HBNB_TYPE_STORAGE') == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
