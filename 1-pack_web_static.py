e3ecca27aad8a48fb8e2a84c407e1b4367acf281
RamzyAR7 <rameya683@gmail.com>
2024-05-04 17:12:13 +0300

    add v1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static folder
+"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    try:
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
+        
+        time_now = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(time_now)
+        local("tar -cvzf {} web_static".format(archive_path))
+        
+        if os.path.exists(archive_path):
+            return archive_path
+        else:
+            return None
+    except:
+        return None
