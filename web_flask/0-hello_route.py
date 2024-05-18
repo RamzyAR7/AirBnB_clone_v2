87153496033c6f6b97bcd4e18eb26b175257f3d8
RamzyAR7 <rameya683@gmail.com>
2024-05-19 00:13:28 +0300

    add v0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 my_app = Flask(__name__)
 
+
 @my_app.route("/", strict_slashes=False)
 def Hello():
     """
     Returns a string 'Hello HBNB!'
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     my_app.run(host="0.0.0.0", port=5000)
