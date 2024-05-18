9b8088669ae1bb442bbbad497a875c58aaa150a5
RamzyAR7 <rameya683@gmail.com>
2024-05-19 00:18:05 +0300

    add v1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A simple Flask web application.
+"""
+from flask import Flask
+
+my_app = Flask(__name__)
+
+
+@my_app.route("/", strict_slashes=False)
+def Hello():
+    """
+    Returns a string 'Hello HBNB!'
+    """
+    return "Hello HBNB!"
+
+
+@my_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    display “HBNB”
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    my_app.run(host="0.0.0.0", port=5000)
