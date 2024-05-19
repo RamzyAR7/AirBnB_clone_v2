ede25255770155dd338dfdccb3a699e5647c3833
RamzyAR7 <rameya683@gmail.com>
2024-05-20 02:34:04 +0300

    add v7

--- models/state.py
+++ models/state.py
@@ -14,10 +14,12 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City",
-                               backref=backref("state", cascade="all"),
-                               cascade="all, delete-orphan",
-                               single_parent=True)
+        cities = relationship(
+            "City",
+            backref=backref("state", cascade="all"),
+            cascade="all, delete-orphan",
+            single_parent=True
+        )
     else:
         name = ""
 
