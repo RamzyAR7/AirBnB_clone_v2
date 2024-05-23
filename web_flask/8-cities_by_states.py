cf50ae434a179424cc5f9a5441b91a36113b1eef
RamzyAR7 <rameya683@gmail.com>
2024-05-23 16:48:49 +0300

    add v9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,7 +5,7 @@ A simple Flask web application.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
+from models.city import City
 
 app = Flask(__name__)
 
@@ -16,11 +16,11 @@ def states_list():
     Displays an HTML page with a list of all State objects present
     in DBStorage,
     sorted by name (A->Z).
-    """
+    Display a HTML page with the list of states"""
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
+    cities = storage.all(City).values()
+    
+    return render_template('7-states_list.html', states=states, cities=cities)
 
 @app.teardown_appcontext
 def teardown_db(exception):
