4a191d9243d17ec0010943612ce041616bc69b8e
RamzyAR7 <rameya683@gmail.com>
2024-05-23 17:24:26 +0300

    add v9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,7 +19,7 @@ def teardown_db(exception=None):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def states_list():
     """
     Displays an HTML page with a list of all State objects present
