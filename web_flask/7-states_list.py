627c30ea82a56f72c235f1530d7bdde6c43b3476
RamzyAR7 <rameya683@gmail.com>
2024-05-23 10:04:33 +0300

    add v7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,9 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
@@ -14,10 +16,12 @@ def states_list():
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Close the storage session after each request"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
